f96b65e68dcbd8e5dbafdf2571f4bf42f2e604ee
Максим <zimovets3@yandex.ru>
2023-01-27 23:07:04 +0300

    Homework №3

---
+++ TS1/Program.cs
@@ -0,0 +1,39 @@
+﻿
+int InputNum(int message)
+{
+    System.Console.Write($"{message}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+bool CheckNum(int num)
+{
+    if (num > 9999 && num < 100000)
+    {
+        return true;
+    }
+    return false;
+    Console.WriteLine("Некорректное число!");
+}
+
+int Calculate(int number)
+{
+    int step1 = number / 10000;
+    int step2 = number % 10;
+    if (step1 == step2)
+    {
+        number = number / 10;
+        int step3 = number / 100;
+        int step4 = number % 10;
+        if (step3 == step4)
+        {
+            System.Console.WriteLine($"{number} является палиндромом");
+        }
+    }
+    else
+    {
+        System.Console.WriteLine($"{number} не является палиндромом");
+    }
+}
+
+
+    
